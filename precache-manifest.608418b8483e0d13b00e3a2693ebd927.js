eb2fc02c7fbb014c845173313241f85547eb45e1
RanningMan <xiaranforusa@gmail.com>
2020-10-03 20:32:49 -0700

    Updates

---
+++ precache-manifest.608418b8483e0d13b00e3a2693ebd927.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f003de6433a615d0acc2aaf69d6348ec",
+    "url": "/UglyTetris/index.html"
+  },
+  {
+    "revision": "d4ff23d8b6b401c785c3",
+    "url": "/UglyTetris/static/css/main.32679f04.chunk.css"
+  },
+  {
+    "revision": "b3c7778b748db380659a",
+    "url": "/UglyTetris/static/js/2.c6b8b11c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/UglyTetris/static/js/2.c6b8b11c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d4ff23d8b6b401c785c3",
+    "url": "/UglyTetris/static/js/main.ec5311ea.chunk.js"
+  },
+  {
+    "revision": "40567aa8a0e5e6ef3c32",
+    "url": "/UglyTetris/static/js/runtime-main.e4056ca7.js"
+  }
+]);
